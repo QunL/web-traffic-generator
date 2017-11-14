1716f7c16ad7a3824b62859dfa2c649f4116159a
QunL <qun.li@foxmail.com>
2017-11-14 14:19:00 +0800

    Isolate the class surf_web

--- gen.py
+++ gen.py
@@ -8,10 +8,8 @@
 #
 #20170714 shyft ADDED python 2.7 and 3.x compatibility and generic config
 from __future__ import print_function 
-import requests, re, time, random 
 import logging
-from HTMLParser import HTMLParser
-import datetime
+from surf_web import SurfWeb
 
 try:
 	import config
@@ -40,185 +38,10 @@ except ImportError:
 		userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) ' \
 			'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
 	config = ConfigClass 
-def remedyUrl(url, newUrl):
-	#print("bef:"+newUrl)
-	if newUrl.startswith('//'):
-		hbeg = url.find('//')
-		if  hbeg == -1:
-			logging.getLogger(self._logEntity).error('getLinksRe cannot find // in page[%s] with new ULR [%s]' % (url, newUrl))
-			return ''
-		newUrl = url[0:hbeg]+newUrl
-	elif newUrl.startswith('/'):
-		hbeg = url.find('//')
-		if  hbeg == -1:
-			logging.getLogger(self._logEntity).error('getLinksRe cannot find // in page[%s] with new ULR [%s]' % (url, newUrl))
-			return ''
-		pbeg = url.find('/', hbeg+2)
-		if  pbeg == -1:
-			newUrl = url + newUrl
-		else :
-			newUrl = url[0:pbeg]+newUrl
-	elif newUrl.startswith('?'):
-		newUrl = url + newUrl
-	#print("aft:"+newUrl)
-	return newUrl
-	
-def testRemedyUrl():
-	assert(remedyUrl("http://abc.com", "//cdf.com/a?b") == "http://cdf.com/a?b")
-	assert(remedyUrl("https://abc.com", "//cdf.com/a?b") == "https://cdf.com/a?b")
-	assert(remedyUrl("https://abc.com", "/cdf.com/a?b") == "https://abc.com/cdf.com/a?b")
-	assert(remedyUrl("https://abc.com/", "/cdf.com/a?b") == "https://abc.com/cdf.com/a?b")
-	assert(remedyUrl("https://abc.com/a.html", "?cdf/a?b") == "https://abc.com/a.html?cdf/a?b")
-	assert(remedyUrl("https://abc.com", "http://cdf.com/a?b") == "http://cdf.com/a?b")
-class surf_web():
-	def __init__(self):
-		self._dataMeter = 0
-		self._goodRequests = 0
-		self._badRequests = 0
-		self._minWait = 3
-		self._maxWait = 15
-		self._userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) ' \
-			'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
-		self._urlTimeout = 15
-		self._logEntity = "http"
-		self._blacklist = []
-		self._width = 5
-		self._depth = 5
-		pass
-
-	def doRequest(self, url):
-		sleepTime = random.randrange(config.minWait,config.maxWait)
-		headers = {'user-agent': config.userAgent}
-		try:
-			r = requests.get(url, headers=headers, timeout=self._urlTimeout)
-		except requests.exceptions.RequestException as e:
-			logging.getLogger(self._logEntity).error('HTTPError[%s] to [%s]'%(str(e), url))
-			return False
-		except :
-			import traceback
-			logging.getLogger(self._logEntity).error('Http 2[%s] exception: %s'%(url, traceback.format_exc()))
-			return False
-			
-		status = r.status_code
-		pageSize = len(r.content)
-		logging.getLogger(self._logEntity).debug("doRequest[%s]status[%s]len[%d]" % (url, status, pageSize))
-		self._dataMeter = self._dataMeter + pageSize
-		if ( status != 200 ):
-			self._badRequests+=1
-			logging.getLogger(self._logEntity).info("Response status[%s]for[%s]" % (r.status_code, url))
-		else:
-			self._goodRequests+=1
-			
-		time.sleep(sleepTime)
-		return r
-
-	def getLinksRe(self, url, page_content):
-		links1=set()
-		links2=set()
-
-		#pattern=r"(?:href\=\")(https?:\/\/[^\"]+)(?:\")"
-		pattern=r"(?:href\=\")(https?:\/\/[^\"]+|(?:\/?\/)[^\"]+|(?:\?)[^\"]+)(?:\")"
-		#print(page_content)
-		matches = re.findall(pattern,str(page_content))
-		
-		for match in matches: # check all matches against config.blacklist
-			if any(bl in match for bl in self._blacklist):
-				pass
-			else:
-				try:
-					h = HTMLParser()
-					match = h.unescape(match)
-				except:
-					import traceback
-					logging.getLogger(self._logEntity).error('HTMLParser.unescape 2[%s] exception: %s'%(url, traceback.format_exc()))
-					continue
-
-				#print("URL1:%s"%str(match))
-				match = remedyUrl(url, match)
-				if match == '':
-					continue
-				links1.add(match)
-		return links1
-	
-	def getLinks(self, url, page_content):
-		return self.getLinksRe(url, page_content)
-	# browse the url and click width+random urls and depth in the every url.    
-	def _browseUrl(self, url, width, depth):
-		logging.getLogger(self._logEntity).debug("_browseUrl:%s width %d depth %d"%(url, width, depth))
-		page = self.doRequest(url)  # hit current root URL
-		if depth <= 0 : return
-		if width <= 0 : return
-		if page:
-			links = self.getLinks(url, page.content) # extract links from page
-			linkCount = len(links)
-		else:
-			logging.getLogger(self._logEntity).error("Error requesting %s" % url)
-			return
-			
-		linkCount = len(links)
-		if linkCount < width:
-			realWidth = linkCount
-		else:
-			realWidth = width
-		for i in range(1, realWidth):
-			clickLink = random.choice(tuple(links))
-			logging.getLogger(self._logEntity).debug("Random[%d/%d]get[%s]in[%s]:%d" % (i, realWidth, clickLink, url, linkCount))
-			self._browseUrl(clickLink, width, depth-1)
-	def browseUrl(self, url):
-		self._browseUrl(url, self._width, self._depth)
-def test_geturl_re():
-	test_str = """<li><a href="https://forums.craigslist.org/?areaID=15&amp;forumID=3232"><span class="txt">apple<sup class="c"></sup></span></a></li>
-	<li><a href="https://forums.craigslist.org/?areaID=15&amp;forumID=49"><span class="txt">arts<sup class="c"></sup></span></a></li>
-	<li><a href="https://forums.craigslist.org/?areaID=15&amp;forumID=575"><span class="txt">haiku<sup class="c"></sup></span></a></li>"""
-	test_str1 = '<a class="channel__subnav__nav-item" href="/channel/longreads">'
-	test_str2 = '		   <a href="http://accounts.craigslist.org/login/home">account</a>'
-	test_str3 = '<a href=" https://www.ebay.com/rpp/gift-cards" _sp="m570.l6463" class="gh-p"> Gift Cards</a>'
-	sw = surf_web()
-	sw.getLinks('abc.com', test_str)
-	sw.getLinks('abc.com', test_str2)
-	sw.getLinks('abc.com', test_str3)
-def getLinksBS(url, page_content):
-	from BeautifulSoup import BeautifulSoup, SoupStrainer
-	links2=set()
-
-	for link in BeautifulSoup(page_content).findAll('a', href=True): #, parseOnlyThese=SoupStrainer('a')):
-		#print("link:"+str(link))
-		if link.get('href') == '#': continue
-		if link.get('href').startswith('javascript'): continue
-		#print("URL2:"+link.get('href'))
-		links2.add(remedyUrl(url, link.get('href')))
-	return links2
-
-def getLinksTestDiff(url):
-	sw = surf_web()
-	page = sw.doRequest(url)  # hit current root URL
-	begtime = datetime.datetime.now()
-	sw = surf_web()
-	links1 = sw.getLinksRe(url, page.content)
-	endtime = datetime.datetime.now()
-	print("Re use time: %s"%str(endtime - begtime))
-	print("############################################")
-	begtime = datetime.datetime.now()
-	links2 = getLinksBS(url, page.content);
-	endtime = datetime.datetime.now()
-	print("BQ use time: %s"%str(endtime - begtime))
-	Only2 = 0
-	for link in links2:
-		if (link not in links1):
-			print("Only2:"+link)
-			Only2 += 1
-	print("#############")
-	Only1 = 0
-	for link in links1:
-		if link not in links2:
-			print("Only1:"+ link)
-			Only1 += 1
-	print("Got URL NUM %d:%d Only:%d:%d"%(len(links1),len(links2), Only1, Only2))
-	return links2
 	
 def browse(urls):
 	currURL = 1
-	sw = surf_web()
+	sw = SurfWeb()
 	sw._maxWait = config.maxWait
 	sw._minWait = config.minWait
 	sw._blacklist = config.blacklist
@@ -228,24 +51,19 @@ def browse(urls):
 		urlCount = len(urls)
 
 		logging.getLogger('main').debug("Request root URL:%s"%url)
-		sw.browseUrl(url)  # hit current root URL
+		sw.browse_url(url)  # hit current root URL
 		if config.debug:
-			if ( sw._dataMeter > 1000000 ):
-				print("Data meter: %s MB" % (sw._dataMeter / 1000000))
+			if ( sw.data_meter > 1000000 ):
+				print("Data meter: %s MB" % (sw.data_meter / 1000000))
 			else:
-				print("Data meter: %s bytes" % sw._dataMeter)
-			print("Good requests: %s" % sw._goodRequests)
-			print("Bad reqeusts: %s" % sw._badRequests)
+				print("Data meter: %s bytes" % sw.data_meter)
+			print("Good requests: %s" % sw.good_requests)
+			print("Bad reqeusts: %s" % sw.bad_requests)
 			
 	logging.getLogger('main').debug("Done.")
 
 #while True:
 if True:
-	'''testRemedyUrl()
-	test_geturl_re()
-	getLinksTestDiff("https://digg.com")
-	getLinksTestDiff("https://cnn.com")
-	exit() '''
 	print("Traffic generator started...")
 	print("----------------------------")
 	print("https://github.com/ecapuano/web-traffic-generator")
@@ -267,5 +85,5 @@ if True:
 	ch.setFormatter(formatter)
 	# add the handlers to logger
 	logging.getLogger('main').addHandler(ch)
-	logging.getLogger('http').addHandler(ch)
+	logging.getLogger('surfweb').addHandler(ch)
 	browse(config.rootURLs)
---
+++ surf_web.py
@@ -0,0 +1,255 @@
+#!/usr/bin/python
+#
+# written by liqun@ncl
+# https://github.com/QunL/web-traffic-generator
+#
+# based on https://github.com/ecapuano/web-traffic-generator
+#20170714 shyft ADDED python 2.7 and 3.x compatibility and generic config
+'''
+The module surf_web is a class simulate surfing web from root url
+click random links and following the link random clicks until certain depth.
+'''
+from __future__ import print_function
+import re
+import time
+import random
+import datetime
+import logging
+import HTMLParser
+import requests
+
+LOG_ENTITY = 'surfweb'
+def remedy_url(url, new_url):
+    ''' The fund remedy new_url retrieved from url page. The new_url might be
+    //cef.com/, /a.thml, ?a=c, etc. '''
+    #print("bef:"+new_url)
+    if new_url.startswith('//'):
+        hbeg = url.find('//')
+        if  hbeg == -1:
+            logging.getLogger(LOG_ENTITY).error('get_links_re cannot '\
+                'find // in page[%s] with new ULR [%s]', url, new_url)
+            return ''
+        new_url = url[0:hbeg]+new_url
+    elif new_url.startswith('/'):
+        hbeg = url.find('//')
+        if  hbeg == -1:
+            logging.getLogger(LOG_ENTITY).error('get_links_re cannot '\
+                'find // in page[%s] with new ULR [%s]', url, new_url)
+            return ''
+        pbeg = url.find('/', hbeg+2)
+        if pbeg == -1:
+            new_url = url + new_url
+        else:
+            new_url = url[0:pbeg]+new_url
+    elif new_url.startswith('?'):
+        new_url = url + new_url
+    #print("aft:"+new_url)
+    return new_url
+
+def test_remedy_url():
+    ''' The func test remedy_url. '''
+    assert(remedy_url("http://abc.com", "//cdf.com/a?b")
+           == "http://cdf.com/a?b")
+    assert(remedy_url("https://abc.com", "//cdf.com/a?b")
+           == "https://cdf.com/a?b")
+    assert(remedy_url("https://abc.com", "/cdf.com/a?b")
+           == "https://abc.com/cdf.com/a?b")
+    assert(remedy_url("https://abc.com/", "/cdf.com/a?b")
+           == "https://abc.com/cdf.com/a?b")
+    assert(remedy_url("https://abc.com/a.html", "?cdf/a?b")
+           == "https://abc.com/a.html?cdf/a?b")
+    assert(remedy_url("https://abc.com", "http://cdf.com/a?b")
+           == "http://cdf.com/a?b")
+class SurfWeb(object):
+    '''The class simulates surfing web from root url click random links and
+    following the link random clicks until certain depth.'''
+    def __init__(self):
+        self.data_meter = 0
+        self.good_requests = 0
+        self.bad_requests = 0
+        self.min_wait = 3
+        self.max_wait = 15
+        self.user_agant = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) '\
+            'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 '\
+            'Safari/537.36'
+        self.url_timeout = 1
+        self.log_entity = LOG_ENTITY
+        self.black_list = []
+        self.width = 2
+        self.depth = 2
+
+    def do_request(self, url):
+        ''' The func request the url, return the request.response.'''
+        sleep_time = random.randrange(self.min_wait, self.max_wait)
+        headers = {'user-agent': self.user_agant}
+        try:
+            req = requests.get(url, headers=headers, timeout=self.url_timeout)
+        except requests.exceptions.RequestException as exp:
+            logging.getLogger(self.log_entity).error('HTTPError[%s] to [%s]',
+                                                     str(exp), url)
+            return False
+
+        status = req.status_code
+        page_size = len(req.content)
+        logging.getLogger(self.log_entity).debug(
+            "do_request[%s]status[%s]len[%d]", url, status, page_size)
+        self.data_meter = self.data_meter + page_size
+        if status != 200:
+            self.bad_requests += 1
+            logging.getLogger(self.log_entity).info(
+                "Response status[%s]for[%s]", req.status_code, url)
+        else:
+            self.good_requests += 1
+
+        time.sleep(sleep_time)
+        return req
+
+    def get_links_re(self, url, page_content):
+        ''' The func return the link list in page_content from url by re.'''
+        links1 = set()
+
+        #pattern=r"(?:href\=\")(https?:\/\/[^\"]+)(?:\")"
+        pattern = r"(?:href\=\")(https?:\/\/[^\"]+|(?:\/?\/)[^\"]+|(?:\?)[^\"]+)(?:\")"
+        #print(page_content)
+        matches = re.findall(pattern, str(page_content))
+
+        for match in matches: # check all matches against self.black_list
+            if any(bl in match for bl in self.black_list):
+                pass
+            else:
+                try:
+                    hparser = HTMLParser.HTMLParser()
+                    match = hparser.unescape(match)
+                except HTMLParser.HTMLParseError:
+                    import traceback
+                    logging.getLogger(self.log_entity).error(
+                        'HTMLParser.unescape 2[%s] exception: %s',
+                        url, traceback.format_exc())
+                    continue
+
+                #print("URL1:%s"%str(match))
+                match = remedy_url(url, match)
+                if match == '':
+                    continue
+                links1.add(match)
+        return links1
+
+    def get_links(self, url, page_content):
+        ''' The func return the link list in page_content from url.'''
+        return self.get_links_re(url, page_content)
+    # browse the url and click width+random urls and depth in the every url.
+    def browse_url_wd(self, url, width, depth):
+        ''' The func surf the link with width random click in the page and
+        following the link for depth.'''
+        logging.getLogger(self.log_entity).debug(
+            "browse_url_wd:%s width %d depth %d", url, width, depth)
+        page = self.do_request(url)  # hit current root URL
+        if depth <= 0:
+            return
+        if width <= 0:
+            return
+        if page:
+            links = self.get_links(url, page.content)
+            link_count = len(links)
+        else:
+            logging.getLogger(self.log_entity).error(
+                "Error requesting %s", url)
+            return
+
+        link_count = len(links)
+        if link_count < width:
+            real_width = link_count
+        else:
+            real_width = width
+        for i in range(1, real_width):
+            click_link = random.choice(tuple(links))
+            logging.getLogger(self.log_entity).debug(
+                "Random[%d/%d]get[%s]in[%s]:%d", i, real_width, click_link,
+                url, link_count)
+            self.browse_url_wd(click_link, width, depth-1)
+    def browse_url(self, url):
+        ''' The func surf the link with width random click in the page and
+        following the link for depth.'''
+        self.browse_url_wd(url, self.width, self.depth)
+
+# For test and compare.
+def test_geturl_re():
+    '''Test func getLinsRe'''
+    test_str = """<li><a href="https://forums.craigslist.org/?areaID=15&amp;forumID=3232">
+    <span class="txt">apple<sup class="c"></sup></span></a></li>
+    <li><a href="https://forums.craigslist.org/?areaID=15&amp;forumID=49">
+    <span class="txt">arts<sup class="c"></sup></span></a></li>
+    <li><a href="https://forums.craigslist.org/?areaID=15&amp;forumID=575">
+    <span class="txt">haiku<sup class="c"></sup></span></a></li>"""
+    test_str1 = '<a class="channel__subnav__nav-item" href="/channel/longreads">'
+    test_str2 = '  <a href="http://accounts.craigslist.org/login/home">account</a>'
+    test_str3 = '<a href=" https://www.ebay.com/rpp/gift-cards" '\
+        '_sp="m570.l6463" class="gh-p"> Gift Cards</a>'
+    surf_web = SurfWeb()
+    surf_web.get_links('abc.com', test_str)
+    surf_web.get_links('abc.com', test_str1)
+    surf_web.get_links('abc.com', test_str2)
+    surf_web.get_links('abc.com', test_str3)
+def get_links_bs(url, page_content):
+    ''' The func return the link list in page_content from url by bs.'''
+    from BeautifulSoup import BeautifulSoup
+    #, SoupStrainer
+    links2 = set()
+
+    for link in BeautifulSoup(page_content).findAll('a', href=True):
+        #, parseOnlyThese=SoupStrainer('a')):
+        #print("link:"+str(link))
+        if link.get('href') == '#':
+            continue
+        if link.get('href').startswith('javascript'):
+            continue
+        #print("URL2:"+link.get('href'))
+        links2.add(remedy_url(url, link.get('href')))
+    return links2
+
+def test_get_links_diff(url):
+    '''Test func different get_links re with bs'''
+    surf_web = SurfWeb()
+    page = surf_web.do_request(url)  # hit current root URL
+    begtime = datetime.datetime.now()
+    surf_web = SurfWeb()
+    links1 = surf_web.get_links_re(url, page.content)
+    endtime = datetime.datetime.now()
+    print("Re use time: %s"%str(endtime - begtime))
+    print("############################################")
+    begtime = datetime.datetime.now()
+    links2 = get_links_bs(url, page.content)
+    endtime = datetime.datetime.now()
+    print("BQ use time: %s"%str(endtime - begtime))
+    only2 = 0
+    for link in links2:
+        if link not in links1:
+            print("only2:"+link)
+            only2 += 1
+    print("#############")
+    only1 = 0
+    for link in links1:
+        if link not in links2:
+            print("only1:" + link)
+            only1 += 1
+    print("Got URL NUM %d:%d Only:%d:%d"%(len(links1), len(links2), only1, only2))
+    return links2
+
+# For test.
+if __name__ == "__main__":
+    logging.basicConfig(level = logging.DEBUG,
+                        format = '%(asctime)s %(name)-8s %(levelname)-6s %(message)s',
+                        datefmt = '%Y-%m-%d %H:%M:%S',
+                        filename = "log.txt")
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter('%(asctime)s %(name)-8s %(levelname)-6s %(message)s')
+    ch.setFormatter(formatter)
+    # add the handlers to logger
+    logging.getLogger(LOG_ENTITY).addHandler(ch)
+    
+    test_remedy_url()
+    test_geturl_re()
+    test_get_links_diff("https://digg.com")
+    test_get_links_diff("https://cnn.com")
